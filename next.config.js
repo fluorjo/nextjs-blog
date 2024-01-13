3ae2b1579c54be4c309206555eeaa69e1304ad8c
fluorjo <fluorjo@naver.com>
2024-01-13 18:25:02 +0900

    글 상세 페이지

--- next.config.js
+++ next.config.js
@@ -1,9 +1,18 @@
-const removeImports= require('next-remove-imports')()
-
+const removeImports = require('next-remove-imports')();
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-}
+    reactStrictMode: true,
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'vyfkwfwikstziyoxdiap.supabase.co',
+                port: '',
+                pathname: '/storage/v1/object/public/**',
+            },
+        ],
+    },
+};
 
-module.exports = removeImports(nextConfig)
+module.exports = removeImports(nextConfig);
