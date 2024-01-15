97cbdde9b91508af4971de8c296c22ef438922f4
fluorjo <fluorjo@naver.com>
2024-01-15 18:05:25 +0900

    tag  목록. 방문자 뱃지.

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,4 +1,6 @@
 import { cn } from '@/utils/style';
+import { createClient } from '@/utils/supabase/client';
+import { useQuery } from '@tanstack/react-query';
 import Link from 'next/link';
 import { FC } from 'react';
 import { AiFillGithub, AiOutlineClose } from 'react-icons/ai';
@@ -9,7 +11,17 @@ type SidebarProps = {
     isOpen: boolean;
 };
 
+const supabase = createClient();
+
 const Sidebar: FC<SidebarProps> = ({ close, isOpen }) => {
+    const { data: existingCategories } = useQuery({
+        queryKey: ['categories'],
+        queryFn: async () => {
+            const { data } = await supabase.from('Post').select('category');
+            return Array.from(new Set(data?.map((d) => d.category)));
+        },
+    });
+
     return (
         <div
             className={cn(
@@ -27,23 +39,31 @@ const Sidebar: FC<SidebarProps> = ({ close, isOpen }) => {
                 HOME
             </Link>
             <Link
-                href={'/tag'}
+                href={'/tags'}
                 className="w-48 font-medium text-gray-600 hover:underline"
             >
                 TAG
             </Link>
-            <Link
-                href={'/category/dummy'}
-                className="w-48 font-medium text-gray-600 hover:underline"
-            >
-                WEB
-            </Link>
+            {existingCategories?.map((category) => (
+                <Link
+                    key={category}
+                    href={`/category/${category}`}
+                    className="w-48 font-medium text-gray-600 hover:underline"
+                >
+                    {category}
+                </Link>
+            ))}
             <div className="mt-10 flex items-center gap-4">
                 <IconButton
                     Icon={AiFillGithub}
                     component={Link}
                     href={'https://github.com/fluorjo'}
                     target="_blank"
+                />
+                {/*eslint-disable-next-line @next/next/no-img-element*/}
+                <img
+                    src="https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=https%3A%2F%2Fgithub.com%2Fgjbae1212%2Fhit-counter&count_bg=%23000000&title_bg=%23A5A5A5&icon=ethereum.svg&icon_color=%23FFFFFF&title=%EB%B0%A9%EB%AC%B8%EC%9E%90&edge_flat=true"
+                    alt="방문자 뱃지"
                 />
             </div>
         </div>
