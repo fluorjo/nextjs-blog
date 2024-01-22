a5404e1a4455cd94b77e0fd1a367977dbf96285e
fluorjo <fluorjo@naver.com>
2024-01-22 20:33:18 +0900

    와 일단 대충이나마 카테고리 클릭 시 불러오기 성공

--- src/pages/categories/[category].tsx
+++ src/pages/categories/[category].tsx
@@ -1,24 +1,41 @@
-import PostList from '@/components/PostList';
+import PostCard from '@/components/PostCard';
 import { GetServerSideProps } from 'next';
-import { useRouter } from 'next/router';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 type CategoryPostsProps = {
     category: string;
 };
 
 export default function CategoryPosts({ category }: CategoryPostsProps) {
-    const router = useRouter();
+    const [data, setData] = useState([]);
+
     useEffect(() => {
-        // 클라이언트 측에서 페이지 이동 시 데이터를 갱신
-        if (router.query.category !== category) {
-            
+        const fetchData = async () => {
+            try {
+                const response = await fetch(`/api/categories/${category}`);
+                const result = await response.json();
+                            const postsArray = result.posts && Array.isArray(result.posts) ? result.posts : [];
+                setData(postsArray);
+            } catch (error) {
+                console.error('Error fetching data:', error);
+            }
+        };
+        console.log(data);
+        fetchData();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [category]);
 
-        }
-    }, [router.query.category, category]);
-    return <PostList category={category} />;
+    return (
+        <div className="flex flex-col items-center gap-8 pt-20">
+            <h1 className="text-2xl font-medium">[{category}]</h1>
+            <div className="container mx-auto grid grid-cols-2 gap-x-4 gap-y-6 px-4 pb-24 lg:gap-x-7 lg:gap-y-12">
+            {data.map((post) => (
+              <PostCard key={post.id} {...post} />
+                ))}
+            </div>
+        </div>
+    );
 }
-
 export const getServerSideProps: GetServerSideProps<
     CategoryPostsProps
 > = async ({ query }) => {
