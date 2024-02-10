a7bd5705ca9f95fc097c4d6332871ee0ae2d4d74
fluorjo <fluorjo@naver.com>
2024-02-10 09:37:48 +0900

    테스트 목록 추가

--- cypress/e2e/home.cy.ts
+++ cypress/e2e/home.cy.ts
@@ -3,6 +3,29 @@ describe('homepage test', () => {
     cy.visit('/')
   })
   it('사이드바', () => {
+    cy.get('[data-cy="sidebarToggle"]').click();
 
+    cy.contains('HOME');
+    cy.contains('TAG');
+
+    cy.get('[data-cy="githubLink"]').should(
+      'have.attr',
+      'href',
+      'https://github.com/fluorjo',
+    );
+  });
+
+  it('글 목록', () => {
+    cy.get('a[href*="/posts/"]').first().click();
+    cy.url().should('include', '/posts/');
+  });
+  
+  it('푸터', () => {
+    cy.contains('ABOUT ME');
+    cy.contains('fluorjo');
+    cy.get('[data-cy="adminLink"]').click();
+    cy.url().should('include', '/admin');
+    cy.get('[data-cy="writeLink"]').click();
+    cy.url().should('not.be.a', '/write');
   });
 })
