5fd867298044c7ff6595dc42a0fe967d305f5cd6
fluorjo <fluorjo@naver.com>
2024-02-14 14:13:10 +0900

    파일명등 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,6 @@ import Sidebar from '@/components/Sidebar';
 import '@/styles/globals.css';
 import { cn } from '@/utils/style';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import type { AppProps } from 'next/app';
 import { Inter } from 'next/font/google';
 import { ReactNode, useState } from 'react';
 
@@ -12,33 +11,36 @@ const inter = Inter({ subsets: ['latin'] });
 
 const queryClient = new QueryClient();
 
-export default function App({ children }: children:ReactNode) {
+export default function App({ children }: { children: ReactNode }) {
     const [isSidebarOpen, setIsSidebarOpen] = useState(false);
     return (
-        <QueryClientProvider client={queryClient}>
-            <div
-                className={cn(
-                    'flex h-screen w-screen text-sm lg:text-base',
-                    inter.className,
-                )}
-            >
-                <Sidebar
-                    close={() => setIsSidebarOpen(false)}
-                    isOpen={isSidebarOpen}
-                />
-                <div className="flex flex-1 flex-col">
-                    <Header
-                        isSidebarOpen={isSidebarOpen}
-                        setIsSidebarOpen={setIsSidebarOpen}
-                    />
-                    <div className="flex flex-1 flex-col overflow-y-auto">
-                        <main className="flex-1">
-                            <Component {...pageProps} />
-                        </main>
-                        <Footer />
+        <html lang="ko">
+            <head />
+            <body>
+                <QueryClientProvider client={queryClient}>
+                    <div
+                        className={cn(
+                            'flex h-screen w-screen text-sm lg:text-base',
+                            inter.className,
+                        )}
+                    >
+                        <Sidebar
+                            close={() => setIsSidebarOpen(false)}
+                            isOpen={isSidebarOpen}
+                        />
+                        <div className="flex flex-1 flex-col">
+                            <Header
+                                isSidebarOpen={isSidebarOpen}
+                                setIsSidebarOpen={setIsSidebarOpen}
+                            />
+                            <div className="flex flex-1 flex-col overflow-y-auto">
+                                <main className="flex-1">{children}</main>
+                                <Footer />
+                            </div>
+                        </div>
                     </div>
-                </div>
-            </div>
-        </QueryClientProvider>
+                </QueryClientProvider>
+            </body>
+        </html>
     );
 }
