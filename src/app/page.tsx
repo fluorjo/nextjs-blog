3e02f68c69d04248bc9365a1d0f9b023365a2a29
fluorjo <fluorjo@naver.com>
2024-02-15 15:04:06 +0900

    page 마이그레이션

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,25 +2,18 @@ import PostList from '@/components/PostList';
 import { Post } from '@/types';
 import { createClient } from '@/utils/supabase/server';
 import { GetStaticProps } from 'next';
+import { cookies } from 'next/headers';
 
-export const getStaticProps = (async () => {
-    const supabase = createClient({});
-
+export default async function Home() {
+    const supabase = createClient(cookies());
     const { data } = await supabase.from('Post').select('*');
 
-    return {
-        props: {
-            posts:
-                data?.map((post) => ({
-                    ...post,
-                    tags: JSON.parse(post.tags) as string[],
-                })) ?? [],
-        },
-    };
-}) satisfies GetStaticProps<{
-    posts: Post[];
-}>;
-
-export default function Home() {
-    return <PostList />;
+    return (
+        <PostList
+            initialPosts={data?.map((post) => ({
+                ...post,
+                tags: JSON.parse(post.tags) as string[],
+            }))}
+        />
+    );
 }
