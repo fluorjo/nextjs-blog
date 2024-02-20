17ee66c775c9607c5c8d9d14af43a2bddff70bef
fluorjo <fluorjo@naver.com>
2024-02-21 01:02:18 +0900

    api handler migration

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,6 +1,6 @@
 import { Database } from './supabase';
 
-export type postRequest = Database['public']['Tables']['Post']['Insert'];
+export type PostRequest = Database['public']['Tables']['Post']['Insert'];
 
 export type Post = Omit<Database['public']['Tables']['Post']['Row'], 'tags'> & {
     tags: string[]; 
